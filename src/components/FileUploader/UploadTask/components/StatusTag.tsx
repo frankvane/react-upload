b11de9e2b09ec53d96e07bc237d12d3c6228160e
vane <13525442@qq.com>
2025-06-02 08:21:32 +0800

    refactor: 移除UploadStatus.ABORTED状态的显示逻辑，简化状态标签组件

--- src/components/FileUploader/UploadTask/components/StatusTag.tsx
+++ src/components/FileUploader/UploadTask/components/StatusTag.tsx
@@ -3,7 +3,6 @@ import {
   CloseCircleOutlined,
   LoadingOutlined,
   PauseCircleOutlined,
-  StopOutlined,
   SyncOutlined,
 } from "@ant-design/icons";
 import { Tag, Tooltip } from "antd";
@@ -66,12 +65,6 @@ export const getStatusTag = (status: UploadStatus): JSX.Element => {
       return (
         <Tag color="error" icon={<CloseCircleOutlined />}>
           合并失败
-        </Tag>
-      );
-    case UploadStatus.ABORTED:
-      return (
-        <Tag color="orange" icon={<StopOutlined />}>
-          已中断
         </Tag>
       );
     default:
